215c7330d4c3c3ac75c39c357143d953e13bc072
Aurélien Mora <ealrann@gmail.com>
2023-08-14 16:29:24 +0200

    New enum test with model

--- isotropy.lmf.core/src/test/java/isotropy/lm/core/resource/ptree/test/model2java/EnumTest.java
+++ isotropy.lmf.core/src/test/java/isotropy/lm/core/resource/ptree/test/model2java/EnumTest.java
@@ -1,6 +1,7 @@
 package isotropy.lm.core.resource.ptree.test.model2java;
 
 import isotropy.lmf.core.lang.Enum;
+import isotropy.lmf.core.lang.Model;
 import isotropy.lmf.core.resource.ptree.PTreeReader;
 import isotropy.lmf.core.resource.transform.PTreeToJava;
 import org.junit.jupiter.api.Test;
@@ -25,6 +26,28 @@ public class EnumTest
 
 		final var color = roots.get(0);
 		assertTrue(color instanceof Enum<?>);
+
+		final var ecolor = (Enum<?>) color;
+		assertEquals("EColor", ecolor.name());
+		assertEquals(3 , ecolor.literals().size());
+		assertEquals("red", ecolor.literals().get(0));
+		assertEquals("green", ecolor.literals().get(1));
+		assertEquals("blue", ecolor.literals().get(2));
+	}
+
+	@Test
+	public void simpleEnumInModel()
+	{
+		final var textModel = "(Model SimpleEnum (Enum name=EColor literals=red,green,blue))";
+		final var inputStream = new ByteArrayInputStream(textModel.getBytes());
+		final var ptree = treeBuilder.read(inputStream);
+		final var ptreeToJava = new PTreeToJava();
+		final var roots = ptreeToJava.transform(ptree);
+
+		final var model = roots.get(0);
+		assertTrue(model instanceof Model);
+
+		final var color = ((Model) model).enums().get(0);
 
 		final var ecolor = (Enum<?>) color;
 		assertEquals("EColor", ecolor.name());
