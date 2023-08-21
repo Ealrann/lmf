9d9f5fc5360c43e37a51b1b0b37cc02a5dbbfd0c
Aurélien Mora <ealrann@gmail.com>
2023-08-21 13:13:30 +0200

    First test with JavaPoet

--- isotropy.lmf.generator/src/main/java/isotropy/lmf/generator/Main.java
+++ isotropy.lmf.generator/src/main/java/isotropy/lmf/generator/Main.java
@@ -1,12 +1,16 @@
 package isotropy.lmf.generator;
 
+import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
 import isotropy.lmf.core.lang.Model;
 import isotropy.lmf.core.resource.ResourceUtil;
 
+import javax.lang.model.element.Modifier;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class Main
 {
@@ -18,21 +22,51 @@ public class Main
 		System.out.println("modelPath = " + modelPath);
 		System.out.println("targetDir = " + targetDir);
 
-
-
 		final var modelFile = new File(modelPath);
 		final var targetPath = new File(targetDir);
 
-		try (final var modelInputStream = new FileInputStream(modelFile)		) {
+		try (final var modelInputStream = new FileInputStream(modelFile))
+		{
 
 			final var roots = ResourceUtil.loadModel(modelInputStream);
 
 			System.out.println("roots = " + roots);
 
-			roots.stream().map(Model.class::cast);
+			roots.stream()
+				 .map(Model.class::cast);
 
+			TypeSpec iface = TypeSpec.interfaceBuilder("HelloWorld")
+									 //.addModifiers(Modifier.PUBLIC)
+									 .addField(FieldSpec.builder(String.class, "ONLY_THING_THAT_IS_CONSTANT")
+														.addModifiers(Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL)
+														.initializer("$S", "change")
+														.build())
+									 .addMethod(MethodSpec.methodBuilder("beep")
+														  .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
+														  .build())
+									 .build();
 
-		} catch (IOException e) {
+			MethodSpec main = MethodSpec.methodBuilder("main")
+										.addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+										.returns(void.class)
+										.addParameter(String[].class, "args")
+										.addStatement("$T.out.println($S)", System.class, "Hello, JavaPoet!")
+										.build();
+
+			TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+										  .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+										  .addMethod(main)
+										  .addType(iface)
+										  .build();
+
+			JavaFile javaFile = JavaFile.builder("com.example.helloworld", helloWorld)
+										.build();
+
+			javaFile.writeTo(System.out);
+
+		}
+		catch (IOException e)
+		{
 			e.printStackTrace();
 		}
 	}
--- isotropy.lmf.generator/src/main/java/module-info.java
+++ isotropy.lmf.generator/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module isotropy.lmf.generator
 {
 	requires isotropy.lmf.core;
 	requires com.squareup.javapoet;
+	requires java.compiler;
 
 	exports isotropy.lmf.generator;
 }
